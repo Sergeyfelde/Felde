887810b266f6793227a22e9d48f84e26aa23ec26
Sergeyfelde <144931802+Sergeyfelde@users.noreply.github.com>
2023-12-29 21:27:27 +0600

    Update OtborVRazvedky.py

--- OMGTU/Oly/OtborVRazvedky.py
+++ OMGTU/Oly/OtborVRazvedky.py
@@ -4,12 +4,12 @@ for tst in range(1, 11):
         tst = '0' + str(tst)
     with open(f'input_s1_{tst}.txt') as f:
         N = int(f.readline())
-        d_2 = int(math.log(N,2))
+        d_2 = int(math.log(N, 2))
         res = 0
         if d_2 == 0:
             res = 0
         else:
-            res = min(abs(2**d_2-N),abs(2**(d_2+1)-N))
+            res = min(abs(2 ** d_2 - N), abs(2 ** (d_2 + 1) - N))
     with open(f'output_s1_{tst}.txt') as d:
         d = int(d.readline())
         if res == d:
