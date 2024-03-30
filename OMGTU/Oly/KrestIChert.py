c47bad578265a0d5068290a351cfa3482efeda18
Sergeyfelde <144931802+Sergeyfelde@users.noreply.github.com>
2024-03-30 15:57:53 +0600

    Delete OMGTU/Oly/KrestIChert.py

--- OMGTU/Oly/KrestIChert.py
+++
@@ -1,9 +0,0 @@
-print('Введите MaxN:')
-MaxN = int(input())
-res = 0
-for i in range(1, MaxN + 1):
-    for j in range(2, 18):
-        if i % (2 ** j - 1) == 0:
-            res += 1
-res += MaxN
-print(f'Количество комбинаций: {res}')
