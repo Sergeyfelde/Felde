80748b2c5222f66489be3bc90e0edf8acc3d82cf
Sergeyfelde <144931802+Sergeyfelde@users.noreply.github.com>
2024-05-30 20:11:08 +0600

    Create Otgadai number.py

---
+++ OMGTU/Oly/2 sem/Otgadai number.py
@@ -0,0 +1,17 @@
+##Выполнено с Денисом Шелеповым
+from sympy import symbols, Eq, solve
+
+
+uravnenie = 'x'
+n = int(input())
+for i in range(n):
+    s = input()
+    if s[0] in '+-':
+        uravnenie += ' ' + s
+    else:
+        uravnenie = f'({uravnenie}) ' + s
+result = int(input())
+x = symbols('x')
+equal = Eq(eval(uravnenie), result)
+solution = solve(equal, x)
+print(solution[0])
